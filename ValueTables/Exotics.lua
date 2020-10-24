12d640cb97f5460449bb98edf63eb9b133d67d85
bonefraktur <73370902+bonefraktur@users.noreply.github.com>
2020-10-24 01:53:04 +0100

    Create Exotics.lua

---
+++ ValueTables/Exotics.lua
@@ -0,0 +1,26 @@
+local ValueTable = {
+            TierOne = { 
+              [1] = { ['Knife_Name'] = "PROTON", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting/Shop/Unboxing" },
+              [2] = { ['Knife_Name'] = "GILDED", ['Knife_Demand'] = 4, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting/Shop/Unboxing" },
+              [3] = { ['Knife_Name'] = "DOOMBRINGER", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting/Shop/Unboxing" },
+              [4] = { ['Knife_Name'] = "CRESCENDO", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting/Shop/Unboxing" },
+              [5] = { ['Knife_Name'] = "FROSTDRAGON", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting/Shop/Unboxing" },
+              [6] = { ['Knife_Name'] = "OVERSEER", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting/Shop/Unboxing" },
+              [7] = { ['Knife_Name'] = "LAVABLADE", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting/Shop/Unboxing" },
+              [8] = { ['Knife_Name'] = "DARKBLADE", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting/Shop/Unboxing" },
+              [9] = { ['Knife_Name'] = "ICELORD", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting/Shop/Unboxing" },
+              [10] = { ['Knife_Name'] = "FAERIE", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting/Shop/Unboxing" },
+              [11] = { ['Knife_Name'] = "EMERALDKNIGHT", ['Knife_Demand'] = 0, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting/Shop/Unboxing" },
+              [12] = { ['Knife_Name'] = "UNICORN", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting" },
+              [13] = { ['Knife_Name'] = "EARTHENBLADE", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting" },
+              [14] = { ['Knife_Name'] = "STRIKER", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting" },
+              [15] = { ['Knife_Name'] = "EMERALDKNIGHT", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Timed Shop/Unboxing" },
+              [16] = { ['Knife_Name'] = "UNICORN", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting" },
+              [17] = { ['Knife_Name'] = "EARTHENBLADE", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting" },
+              [18] = { ['Knife_Name'] = "STRIKER", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting" },
+              [19] = { ['Knife_Name'] = "PHARAOH", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Timed Shop/Unboxing" }
+           },
+           PackExotics = {
+           [1] = { ['Knife_Name'] = "PROTON", ['Knife_Demand'] = 3, ['Knife_Value'] = 1, ['Knife_Root'] = "Trading/Crafting/Shop/Unboxing" }
+           },
+}
